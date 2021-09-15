0be56ffdbbb48f8cd5229097a0fd3d714d25e283
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-09-15 07:01:42 -0700

    "refactored code"

--- gestures.py
+++ gestures.py
@@ -6,9 +6,7 @@ LIZARD = "lizard"
 
 GESTURES = [ROCK, SCISSORS, PAPER, SPOCK, LIZARD]
 
-# map keyed by gestures and a list of gestures that they win against.
-# i.e. ROCK wins against SCISSORS
-# and SPOCK wins against SCISSORS or LIZARD
+
 WINNING_GESTURES = {
     ROCK: [SCISSORS, LIZARD],
     SCISSORS: [PAPER, LIZARD],
